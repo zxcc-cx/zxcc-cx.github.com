480801ecfc3845b59f539b293f71b95d26da43ad
zxcc-cx <1514434200@qq.com>
2020-11-14 20:41:37 +0800

    添加时间
    添加时间显示的相关代码；

--- js/myscript01.js
+++ js/myscript01.js
@@ -6,3 +6,17 @@ function changeImage(){
 		element.src="image/pic_bulbon.gif";
 	}
  }
+ function jt(){
+	function getDate(){
+		//获取当前时间
+		var date = new Date();
+		//格式化为本地时间格式
+		var date1 = date.toLocaleString();
+		//获取div
+		var div1 = document.getElementById("times");
+		//将时间写入div
+		div1.innerHTML = date1;
+	}
+	//使用定时器每秒向div写入当前时间
+	setInterval("getDate()",1000);
+ }
